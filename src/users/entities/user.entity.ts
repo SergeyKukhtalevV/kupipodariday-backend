46c118866f7b22ce31cb9cb2165e884c096185bd
Sergey Kukhtalev <s.kuhtalev@arktur.su>
2023-10-10 20:07:21 +0300

    feature: added general entity and user's entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,27 @@
-export class User {}
+import { Column, Entity } from 'typeorm';
+import { GeneralEntity } from '../../utils/GeneralEntity';
+import { IsEmail, IsUrl, MaxLength, MinLength } from 'class-validator';
+
+@Entity()
+export class User extends GeneralEntity {
+  @Column({ unique: true })
+  @MinLength(2)
+  @MaxLength(30)
+  username: string;
+
+  @Column({ default: 'Пока ничего не рассказал о себе' })
+  @MinLength(2)
+  @MaxLength(200)
+  about: string;
+
+  @Column({ default: 'https://i.pravatar.cc/300' })
+  @IsUrl()
+  avatar: string;
+
+  @Column({ unique: true })
+  @IsEmail()
+  email: string;
+
+  @Column()
+  password: string;
+}
