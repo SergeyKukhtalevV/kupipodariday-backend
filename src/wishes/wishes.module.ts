283aec96e76aa2fb8c5934b2b7778d30b72e5be3
Sergey Kukhtalev <s.kuhtalev@arktur.su>
2023-10-11 13:15:44 +0300

    feature: imports entities in either module

--- src/wishes/wishes.module.ts
+++ src/wishes/wishes.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { WishesService } from './wishes.service';
 import { WishesController } from './wishes.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Wish } from './entities/wish.entity';
+import { User } from '../users/entities/user.entity';
+import { Offer } from '../offers/entities/offer.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Wish, User, Offer])],
   controllers: [WishesController],
   providers: [WishesService],
 })
