c4ec7f59b05cedfd39e64d5a8291bddefbceb13b
Sergey Kukhtalev <s.kuhtalev@arktur.su>
2023-10-17 16:05:02 +0300

    feature: added wishlist.dto

--- src/wishlists/dto/create-wishlist.dto.ts
+++ src/wishlists/dto/create-wishlist.dto.ts
@@ -1,1 +1,26 @@
-export class CreateWishlistDto {}
+import {
+  IsArray,
+  IsInt,
+  IsString,
+  IsUrl,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateWishlistDto {
+  @MinLength(1)
+  @MaxLength(250)
+  @IsString()
+  name: string;
+
+  @MaxLength(1500)
+  @IsString()
+  description: string;
+
+  @IsUrl()
+  image: string;
+
+  @IsArray()
+  @IsInt({ each: true })
+  items: number[];
+}
--- src/wishlists/dto/update-wishlist.dto.ts
+++ src/wishlists/dto/update-wishlist.dto.ts
@@ -1,4 +1,28 @@
-import { PartialType } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/mapped-types';
 import { CreateWishlistDto } from './create-wishlist.dto';
+import {
+  IsArray,
+  IsInt,
+  IsString,
+  IsUrl,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
-export class UpdateWishlistDto extends PartialType(CreateWishlistDto) {}
+export class UpdateWishlistDto extends PartialType(CreateWishlistDto) {
+  @MinLength(1)
+  @MaxLength(250)
+  @IsString()
+  name: string;
+
+  @MaxLength(1500)
+  @IsString()
+  description: string;
+
+  @IsUrl()
+  image: string;
+
+  @IsArray()
+  @IsInt({ each: true })
+  items: number[];
+}
